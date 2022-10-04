83353b7ef4341d9efad4e20b3fbc8ee71544911a
0zzysome <oscar.rosin@hotmail.com>
2022-10-04 13:30:07 +0200

    jhfbajvfa

--- FightingGame/Fighter.cs
+++ FightingGame/Fighter.cs
@@ -6,13 +6,16 @@ public class Fighter
     public int Class;
     public string ClassName = "no class";
     public string name;
-    public double hp = 100;
+    public double MaxHp;
+    public double hp;
     public int Str;
-    public int Int;
+    public double Dodge;
     public double Def;
     public double CritChance;
     public double CritDmg;
-    private bool MadeChoice = false;
+    public double HealMult;
+    public bool HasHealed= false;
+    private bool MadeChar = false;
     public Weapon weapon;
 
     public Fighter()
@@ -20,54 +23,106 @@ public class Fighter
         //spelaren väljer namn på karatär
         Console.WriteLine("Choose a name for your fighter."); 
         name = Console.ReadLine();
-        while (MadeChoice==false)
+        // ser om skapandet av karaktären gick igenom
+        while (MadeChar==false)
         {
             // skriv ner alla klaser och stats
             // spelaren väljer klass
             Console.WriteLine("Choose a class your fighter."); 
-            MadeChoice =int.TryParse(Console.ReadLine(), out Class);
+            MadeChar =int.TryParse(Console.ReadLine(), out Class);
             // Ändrar på alla variabler så de matchar klassen
             //
             switch(Class)
             {
                 case 1:
+                    // class = 1 från parse
                     ClassName = "Rouge";
-                    Class = 1;
-                    hp = 100;
-                    Str = 10;
-                    Int = 8;
+                    hp = 80;
+                    MaxHp = 80;
+                    Str = 8;
+                    Dodge = 0.3;
                     Def = 0.1;
-                    CritChance = 0.05;
+                    CritChance = 0.25;
                     CritDmg = 2;
+                    weapon.name = "knife";
                     break;
                 case 2: 
+                    // class = 2 från parse
                     ClassName = "fighter";
-                    Class = 2;
-                    hp = 100;
+                    hp = 110;
+                    MaxHp = 110;
                     Str = 10;
-                    Int = 8;
-                    Def = 0.1;
+                    Dodge = 0.1;
+                    Def = 0.3;
                     CritChance = 0.05;
                     CritDmg = 2;
                     break;
                 default:
-                    MadeChoice = false;
+                    MadeChar = false;
                     break;
 
             }
         }
         
 
+
+    }
+
+    public void Action()
+    {
+        bool ChoseAction = false;
+        while (ChoseAction==false)
+        {
+            Console.WriteLine($"What will {name} do?");
+            Console.WriteLine($"1. attack with {weapon.name}.");
+            Console.WriteLine("2. Dodge.");
+            Console.WriteLine("3. Block.");
+            if(HasHealed==false)
+            {
+                Console.WriteLine("4.Heal.");
+            }
+            else
+            {
+                Console.WriteLine($"4.{name} cant heal any more.");          
+            }
+            ChoseAction =int.TryParse(Console.ReadLine(), out Class);
+
+        }
+         
+        
+    }
+    //healing
+    public bool Heal()
+    {
+        if(HasHealed==false)
+        {   
+            hp = hp+(10*HealMult);
+            hp = Math.Min(MaxHp, hp);
+            HasHealed = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
     public void Hit(Fighter target)
     {
+        /*
+        jobbar på senare
+        if(weapon.DoesHit(target.Dodge))
+        {
+            
+        }
+        */
         // formel för att reducera skada
         // dmg = dmg * (1-defence) 
         double damage = weapon.GetDamage();
         // tittar om atacken är en crit
         if (weapon.DoesCrit(CritChance))
         {
-             Console.WriteLine("Critical Hit");
+             Console.WriteLine("Critical Hit!");
              damage = damage*Str*(1-Def)*CritDmg;
         }
         else
@@ -77,6 +132,6 @@ public class Fighter
 
         target.hp -= damage;
         target.hp = Math.Max(0, target.hp);
-        Console.WriteLine($"{name} did {damage} damage to {target.name}.");
+        Console.WriteLine($"{name} hit {target.name} and did {damage} damage.");
     }
 }
--- FightingGame/Weapon.cs
+++ FightingGame/Weapon.cs
@@ -2,16 +2,29 @@ using System;
 
 public class Weapon
 {
-    public string name;
+    
+    public string name = "none";
+    public int HitChance;
     private Random Generator;
     public Weapon()
     {
         Generator = new Random();
-        
-
-
 
     }
+    // ser om slaget träffar (inte klar)
+    public bool DoesHit(double HitChance)
+    {
+        double roll = Generator.NextDouble();
+        if(roll >= HitChance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;   
+        }
+    }
+
     // genererad skadan
     public int GetDamage()
     {
